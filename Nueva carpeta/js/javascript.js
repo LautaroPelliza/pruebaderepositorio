3898b58840c4d65d6f1297e71210242b68833522
Lautaro Pelliza <lautypelliza1@gmail.com>
2024-03-13 20:34:23 -0300

    js actualizado

---
+++ Nueva carpeta/js/javascript.js
@@ -0,0 +1,7 @@
+let platoPrincipal = 'Pizza';
+// aclaracion de una variable
+let platoSecundario = 'Estofado';
+// valor variable
+const restaurant = 'Hard Rock Cafe';
+// valor fijo
+platoPrincipal = 'Pizza Calabresa'
